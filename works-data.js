5dc6c285a7e1cf7ec6756d634f337a693cc93539
bitcan2023-boop <bitcan2023@gmail.com>
2026-06-22 03:25:04 +0900

    Deploy: update works-data.js

--- works-data.js
+++ works-data.js
@@ -12,14 +12,10 @@ const WORKS_DATA = [
     client: "UIMUI",
     description: "UIMUI New Single「感情ワンダーランド」Music Video。",
     thumb: "kanjo-wonderland.png",
-    displayTitle: "UIMUI - MV \"感情ワンダーランド\"",
+    displayTitle: 'UIMUI - MV "感情ワンダーランド"',
     media: [
-        {
-            "type": "youtube",
-            "id": "hKkhIVI3VkI"
-        }
+      { type: "youtube", id: "hKkhIVI3VkI" }
     ],
-    draft: undefined,
   },
 
   {
@@ -33,18 +29,9 @@ const WORKS_DATA = [
     thumb: "FIJタイトル_4.jpg",
     displayTitle: "china town silhouette",
     media: [
-        {
-            "type": "image",
-            "src": "DSC01346.jpg"
-        },
-        {
-            "type": "image",
-            "src": "FIJタイトル_4.jpg"
-        },
-        {
-            "type": "image",
-            "src": "DSC01314.jpg"
-        }
+      { type: "image", src: "DSC01346.jpg" },
+      { type: "image", src: "FIJタイトル_4.jpg" },
+      { type: "image", src: "DSC01314.jpg" }
     ],
   }
 ];
