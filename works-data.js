865b75aed98b77175b0ab0545588cd22fbe3b82e
bitcan2023-boop <bitcan2023@gmail.com>
2026-06-27 20:09:52 +0900

    CMS: update works-data.js (2026-06-27)

--- works-data.js
+++ works-data.js
@@ -78,6 +78,7 @@ const WORKS_DATA = [
     description: "Apple Music,Spotify,etc. 各Streamingサービスにて配信中\n→https://www.tunecore.co.jp/artists?id=1029918\n\ncast：UIMUI\ndirection：HIBIKI\nedit：HIBIKI\nshoot：KURO\n\nRecording&Mixing：So Shibata(Shibata Sound Works)\n\nUIMUI YouTube Official CH\nhttps://bit.ly/4ePDZiY",
     thumb: "しがらみMVサムネ2_2.PNG",
     displayTitle: "UIMUI - MV \"しがらみしがらむ\"",
+    orientation: "portrait",
     media: [
         {
             "type": "youtube",
