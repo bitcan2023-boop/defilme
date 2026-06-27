b81cdd10cd313c1dcda7851a6d56d2cb505be0c2
bitcan2023-boop <bitcan2023@gmail.com>
2026-06-27 20:08:58 +0900

    CMS: update works-data.js (2026-06-27)

--- works-data.js
+++ works-data.js
@@ -72,12 +72,12 @@ const WORKS_DATA = [
     id: "uimui-1st-single--music-video",
     category: "videography",
     sub: "music-video",
-    title: "UIMUI 1st Single \"感情ワンダーランド\" Music Video",
+    title: "UIMUI 1st Single \"しがらみしがらむ\" Music Video",
     date: "2025-09",
     client: "UIMUI",
     description: "Apple Music,Spotify,etc. 各Streamingサービスにて配信中\n→https://www.tunecore.co.jp/artists?id=1029918\n\ncast：UIMUI\ndirection：HIBIKI\nedit：HIBIKI\nshoot：KURO\n\nRecording&Mixing：So Shibata(Shibata Sound Works)\n\nUIMUI YouTube Official CH\nhttps://bit.ly/4ePDZiY",
     thumb: "しがらみMVサムネ2_2.PNG",
-    displayTitle: "UIMUI 1st Single「感情ワンダーランド」Music Video",
+    displayTitle: "UIMUI - MV \"しがらみしがらむ\"",
     media: [
         {
             "type": "youtube",
